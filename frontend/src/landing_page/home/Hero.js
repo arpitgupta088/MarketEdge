e7e864802922b625fd921d0478feeca80768d8f3
arpit gupta <arpitgupta14786@gmail.com>
2025-08-19 17:36:12 +0530

    defined basic structure of each component

---
+++ frontend/src/landing_page/home/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Hero() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    
+                </div>
+                <div className="col">
+                    
+                </div>
+                <div className="col">
+                    
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Hero;
