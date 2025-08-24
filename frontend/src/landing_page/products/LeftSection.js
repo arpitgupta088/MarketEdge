0fba245381da7788d4ec20b7a5c59e17c9ea6d37
arpit gupta <arpitgupta14786@gmail.com>
2025-08-24 14:26:00 +0530

    added products section

--- frontend/src/landing_page/products/LeftSection.js
+++ frontend/src/landing_page/products/LeftSection.js
@@ -1,7 +1,26 @@
 import React from 'react';
-function LeftSection() {
+function LeftSection({imageURL, productName, productDescription, tryDemo, learnMore, googlePlay, appStore}) {
     return ( 
-        <h1>Left section</h1>
+        <div className='container'>
+            <div className="row">
+                <div className='col-6 p-5'>
+                    <img src={imageURL}/>
+                </div>
+                <div className='col-6 p-5 mt-3'>
+                    <h1>{productName}</h1>
+                    <p>{productDescription}</p>
+                    <div>
+                    <a href={tryDemo}>Try Demo</a>
+                    <a href={learnMore} style={{marginLeft:'50px'}}>Learn more</a>
+                    </div>
+                    <div className='mt-3'>
+                        <a href={googlePlay}><img src="media/images/googlePlayBadge.svg" /></a>
+                    <a href={appStore}><img src="media/images/appStoreBadge.svg" style={{marginLeft:'50px'}} /></a>
+                    </div>
+                    
+                </div>
+            </div>
+        </div> 
      );
 }
 
