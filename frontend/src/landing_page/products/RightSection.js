180e116e750b2da0c745936f0b4e39e1c8a4d8fd
arpit gupta <arpitgupta14786@gmail.com>
2025-08-25 14:40:10 +0530

    added right section in products tab

--- frontend/src/landing_page/products/RightSection.js
+++ frontend/src/landing_page/products/RightSection.js
@@ -1,8 +1,31 @@
-import React from 'react';
-function RightSection() {
-    return ( 
-        <h1>RightSection</h1>
-     );
+import React from "react";
+function RightSection({
+  imageURL,
+  productName,
+  productDescription,
+  learnMore,
+}) {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6 p-5 mt-3">
+          <h1>{productName}</h1>
+          <p>{productDescription}</p>
+          <div>
+            <a
+              href={learnMore}
+              style={{ marginLeft: "50px", textDecoration: "none" }}
+            >
+              Learn more
+            </a>
+          </div>
+        </div>
+        <div className="col-6 p-1">
+          <img src={imageURL} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default RightSection;
