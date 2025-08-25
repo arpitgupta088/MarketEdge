30b0cdcbc21f73d799792ddaa5f58fb2034b7404
arpit gupta <arpitgupta14786@gmail.com>
2025-08-25 14:41:39 +0530

    added universe section in products tab

--- frontend/src/landing_page/products/Universe.js
+++ frontend/src/landing_page/products/Universe.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import React from "react";
 
 function Universe() {
-    return ( 
-        <h1>Universe</h1>
-     );
+  return (
+    <div className="container mt-5">
+      <div className="row text-center">
+        <h1 className="mt-5">Apna MarketEdge Universe</h1>
+        <p>
+          Extend your trading and investment experience even further with our
+          partner platforms
+        </p>
+
+        <div className="col-4 p-3 ">
+          <img src="media/images/smallcaseLogo.png" />
+          <p className="text-small text-muted">
+            Thematic investing platform that helps you invest in diversified
+            baskets of stocks on ETFs.
+          </p>
+        </div>
+        <div className="col-4 p-3">
+          <img src="media/images/smallcaseLogo.png" />
+          <p className="text-small text-muted">
+            Options trading platform that lets you create strategies, analyze
+            positions, and examine data points like open interest, FII/DII, and
+            more.
+          </p>
+        </div>
+        <div className="col-4 p-3">
+          <img src="media/images/dittoLogo.png" style={{ width: "27%" }} />
+          <p className="text-small text-muted">
+            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/sensibullLogo.svg" style={{ width: "40%" }} />
+          <p className="text-small text-muted">
+            lorem orem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/streakLogo.png" style={{ width: "40%" }} />
+          <p className="text-small text-muted">
+            orem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div>
+        <button className="p-3 btn btn-primary fs-5 mb-5" style={{width:"20%", margin:"0 auto"}}>Signup Now</button>
+      </div>
+      </div>
+    </div>
+  );
 }
 
 export default Universe;
